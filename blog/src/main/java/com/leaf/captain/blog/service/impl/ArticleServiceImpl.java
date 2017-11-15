302c29ba5f164dda4db47831fc7b6c0951dc87b2
dearmaster windows7 <694114533@qq.com>
2017-11-16 03:04:24 +0800

    .

--- blog/src/main/java/com/leaf/captain/blog/service/impl/ArticleServiceImpl.java
+++ blog/src/main/java/com/leaf/captain/blog/service/impl/ArticleServiceImpl.java
@@ -5,11 +5,13 @@ import com.leaf.captain.blog.model.Article;
 import com.leaf.captain.blog.service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.io.Serializable;
 import java.util.List;
 
 @Service("articleService")
+@Transactional
 public class ArticleServiceImpl implements ArticleService {
 
     @Autowired
